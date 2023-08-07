4ab98dbfa1e7e0982b45bcad5337cc8a81683808
Mynssem <mynssemgabriel@gmail.com>
2023-08-07 11:54:13 -0300

    Possiveis movimentos da peça

---
+++ src/application/Main.java
@@ -0,0 +1,44 @@
+package application;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import javax.xml.transform.Source;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ChessMatch chessMatch = new ChessMatch();
+
+        while (true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch (ChessException | InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+        }
+
+
+    }
+}
