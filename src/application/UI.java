3aac78791bc0a5a63bee34504e3e51eb72d2302e
Mynssem <mynssemgabriel@gmail.com>
2023-08-15 09:28:28 -0300

    Movimentação da peça BISHOP

--- src/application/UI.java
+++ src/application/UI.java
@@ -114,7 +114,7 @@ public class UI {
         System.out.print(ANSI_RESET);
 
         System.out.print("Black: ");
-        System.out.print(ANSI_YELLOW);
+        System.out.print(ANSI_RED);
         System.out.println(Arrays.toString(black.toArray()));
         System.out.print(ANSI_RESET);
     }
